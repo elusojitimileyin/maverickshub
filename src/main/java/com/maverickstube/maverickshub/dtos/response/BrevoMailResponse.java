e0fee9ee358f4625f5d6ebb207b657e020daf15f
Elusoji Timileyin <elusojitimileyin@gmail.com>
2024-07-01 18:30:19 +0100

    updated

---
+++ src/main/java/com/maverickstube/maverickshub/dtos/response/BrevoMailResponse.java
@@ -0,0 +1,11 @@
+package com.maverickstube.maverickshub.dtos.response;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@Getter
+
+public class BrevoMailResponse {
+    private String messageId;
+}
--- src/main/java/com/maverickstube/maverickshub/services/MavericksHubMailService.java
+++ src/main/java/com/maverickstube/maverickshub/services/MavericksHubMailService.java
@@ -5,12 +5,20 @@ import com.maverickstube.maverickshub.dtos.requests.BrevoMailRequest;
 import com.maverickstube.maverickshub.dtos.requests.Recipient;
 import com.maverickstube.maverickshub.dtos.requests.SendMailRequest;
 import com.maverickstube.maverickshub.dtos.requests.Sender;
+import com.maverickstube.maverickshub.dtos.response.BrevoMailResponse;
 import lombok.AllArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.MediaType;
+import org.springframework.http.RequestEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
+import java.net.URI;
 import java.util.List;
+
+import static org.springframework.http.MediaType.APPLICATION_JSON;
 
 @Service
 @AllArgsConstructor
@@ -28,8 +36,13 @@ public class MavericksHubMailService implements MailService {
         request.setSubject(mailRequest.getSubject());
         request.setRecipients(List.of(
                 new Recipient(mailRequest.getRecipientEmail(),mailRequest.getRecipientName())));
-        request.setContent();
-        restTemplate.postForEntity(url,request,);
+        request.setContent(mailRequest.getContent());
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(APPLICATION_JSON);
+        headers.set("api-key",mailConfig.getMailApiKey());
+        headers.set("accept",APPLICATION_JSON.toString());
+        RequestEntity<?> httpRequest = new RequestEntity<>(request,headers, HttpMethod.POST, URI.create(url));
+        restTemplate.postForEntity(url,request, BrevoMailResponse.class);
         return "";
     }
 
