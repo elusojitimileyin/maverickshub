c8f85b1be703ca1c601fef8de025281c4c9ab013
Elusoji Timileyin <elusojitimileyin@gmail.com>
2024-07-01 14:23:39 +0100

    updated

---
+++ src/main/java/com/maverickstube/maverickshub/security/filters/CustomAuthorizationFilter.java
@@ -0,0 +1,24 @@
+package com.maverickstube.maverickshub.security.filters;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+import java.util.logging.Filter;
+
+@Component
+public class CustomAuthorizationFilter extends OncePerRequestFilter {
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request,
+                                    HttpServletResponse response,
+                                    FilterChain filterChain) throws ServletException, IOException {
+
+
+
+    }
+}
